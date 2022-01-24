cc4f6fdbab74fe5c619f12adef97a037e4c3545b
Alison Zhao <alisonzhao@MacBook.local>
2022-01-23 21:48:32 -0800

    feat(contact): add contact info to contact page

--- src/pages/ContactPg.js
+++ src/pages/ContactPg.js
@@ -1,10 +1,16 @@
 import classes from './ContactPg.module.css';
 import Contact from '../components/Layout/Contact';
-import QuoteForm from '../components/QuoteForm';
+
 const ContactPg = () => {
   return (
     <div className={classes.contact}>
-      <Contact />
+      <h2 className='headingSecondary'>Contact us</h2>
+
+      <p className={classes.contacts}>
+        <p> 趙國榮 Gordon Zhao</p>
+        <p> 微信號 tel: (415) 971-6316</p>
+        <p> e-mail: sffuring@yahoo.com</p>
+      </p>
     </div>
   );
 };
